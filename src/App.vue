92bc9550d0b51b71a9816da9ef63485c35ab4e7d
kevangi-zignuts <kevangip@zignuts.com>
2024-06-10 14:40:12 +0530

    Add a Component for Navbar. Include navbar in needed file using app.vue Add Logout in navbar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,37 @@
 <script setup>
-import { RouterView } from 'vue-router';
+import { RouterView, useRoute, useRouter } from 'vue-router';
+import { onMounted, ref, watch } from "vue";
+import Navbar from '@/components/Navbar.vue';
+
+const route = useRoute();
+const router = useRouter();
+
+let showNavbar = ref(true);
+
+// Function to update showNavbar based on route meta field
+const updateShowNavbar = (route) => {
+  if (route.meta && route.meta.showNavbar !== undefined) {
+    showNavbar.value = route.meta.showNavbar;
+  } else {
+    showNavbar.value = true; // Default to true if no meta field is set
+  }
+};
+
+// Check initial route
+onMounted(() => {
+  updateShowNavbar(route);
+});
+
+// Watch for route changes
+watch(route, (newRoute) => {
+  updateShowNavbar(newRoute);
+});
 </script>
 
 <template>
+  <div v-if="showNavbar">
+    <Navbar />
+  </div>
   <RouterView />
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -5,47 +5,74 @@ const routes = [
         path: '/',
         name: 'login',
         component: () => import('@/components/auth/Login.vue'),
+        meta: {
+            showNavbar: false
+        }
     },
     {
         path: '/register',
         name: 'register',
         component: () => import('@/components/auth/Register.vue'),
+        meta: {
+            showNavbar: false
+        }
     },
     {
         path: '/forgot-password',
         name: 'forgotPassword',
         component: () => import('@/components/auth/ForgotPassword.vue'),
-    },
-    {
-        path: '/task',
-        name: 'list',
-        component: () => import('@/components/tasks/List.vue'),
-    },
-    {
-        path: '/task/create',
-        name: 'create',
-        component: () => import('@/components/tasks/Create.vue'),
-    },
-    {
-        path: '/task/view/:id(\\d+)',
-        name: 'view',
-        component: () => import('@/components/tasks/View.vue'),
-    },
-    {
-        path: '/task/edit/:id(\\d+)',
-        name: 'edit',
-        component: () => import('@/components/tasks/Edit.vue'),
+        meta: {
+            showNavbar: false
+        }
     },
     {
         path: '/reset-password/:token',
         name: 'resetPassword',
         component: () => import('@/components/auth/ResetPassword.vue'),
+        meta: {
+            showNavbar: false
+        }
+    },
+    {
+        path: '/task',
+        name: 'list',
+        component: () => import('@/components/tasks/List.vue'),
+        meta: {
+            showNavbar: true
+        }
+    },
+    {
+        path: '/task/create',
+        name: 'create',
+        component: () => import('@/components/tasks/Create.vue'),
+        meta: {
+            showNavbar: true
+        }
+    },
+    {
+        path: '/task/view/:id(\\d+)',
+        name: 'view',
+        component: () => import('@/components/tasks/View.vue'),
+        meta: {
+            showNavbar: true
+        }
+    },
+    {
+        path: '/task/edit/:id(\\d+)',
+        name: 'edit',
+        component: () => import('@/components/tasks/Edit.vue'),
+        meta: {
+            showNavbar: true
+        }
     },
     // 404 Page not found
     {
         path: '/:pathMatch(.*)*',
         name: 'notfound',
         component: () => import('@/views/PageNotFound.vue'),
+        meta: {
+            showNavbar: false
+        }
     },
 ]
 
